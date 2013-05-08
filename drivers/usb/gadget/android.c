55dbf4ac547f343db96201384d1b0fbde5296e2d
Matt Koenig <der_koenig@rocketmail.com>
2013-05-08 22:14:44 +0200

    Change UMS LUN count to 2
    Change-Id: I76e53174b56ca35b396d59083de1df432714bdc1

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1071,24 +1071,26 @@ static int mass_storage_function_init(struct android_usb_function *f,
 	struct fsg_common *common;
 	int err;
 	int i;
-	const char *name[2];
+	const char *name[3];
 
 	config = kzalloc(sizeof(struct mass_storage_function_config),
 								GFP_KERNEL);
 	if (!config)
 		return -ENOMEM;
 
-	config->fsg.nluns = 1;
+	config->fsg.nluns = 2;
 	name[0] = "lun";
+	name[1] = "lun0";
 	if (dev->pdata->cdrom) {
-		config->fsg.nluns = 2;
-		config->fsg.luns[1].cdrom = 1;
-		config->fsg.luns[1].ro = 1;
-		config->fsg.luns[1].removable = 0;
-		name[1] = "lun0";
+		config->fsg.nluns = 3;
+		config->fsg.luns[2].cdrom = 1;
+		config->fsg.luns[2].ro = 1;
+		config->fsg.luns[2].removable = 0;
+		name[2] = "lun1";
 	}
 
 	config->fsg.luns[0].removable = 1;
+	config->fsg.luns[1].removable = 1;
 
 	common = fsg_common_init(NULL, cdev, &config->fsg);
 	if (IS_ERR(common)) {
